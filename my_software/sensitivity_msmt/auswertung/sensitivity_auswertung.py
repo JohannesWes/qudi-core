6e070c3b34b430079a1d092fed2a7977b0b7556f
Johannes Wesseler <johannes.wesseler@gmail.com>
2024-04-20 17:26:58 +0200

    fixes and refactoring

--- my_software/sensitivity_msmt/auswertung/sensitivity_auswertung.py
+++ my_software/sensitivity_msmt/auswertung/sensitivity_auswertung.py
@@ -45,19 +45,20 @@ peaks_indices, dips_indices, zero_crossings_indices, slopes, intercepts = evalua
 
 # --- Plot hyperfine spectrum and fitted lines --- #
 fig, ax = plt.subplots()
-ax.plot(odmr_data["Frequency"], odmr_data["Voltage"])
-ax.scatter(odmr_data['Frequency'].iloc[peaks_indices], odmr_data['Voltage'].iloc[peaks_indices], color='red')
-ax.scatter(odmr_data['Frequency'].iloc[dips_indices], odmr_data['Voltage'].iloc[dips_indices], color='blue')
+ax.plot(odmr_data["Frequency"]/1e6, odmr_data["Voltage"], linewidth=3, color="dimgray")
+ax.scatter(odmr_data['Frequency'].iloc[peaks_indices]/1e6, odmr_data['Voltage'].iloc[peaks_indices], color='red', label="Peaks", zorder=3)
+ax.scatter(odmr_data['Frequency'].iloc[dips_indices]/1e6, odmr_data['Voltage'].iloc[dips_indices], color='cornflowerblue', label="Dips", zorder=3)
 
 for i, slope in enumerate(slopes):
-    lower_bound, upper_bound = zero_crossings_indices[i] - 20, zero_crossings_indices[i] + 20
-    ax.plot(odmr_data['Frequency'][lower_bound:upper_bound],
-            slopes[i] * odmr_data['Frequency'][lower_bound:upper_bound] + intercepts[i], color='green')
+    lower_bound, upper_bound = zero_crossings_indices[i] - 30, zero_crossings_indices[i] + 30
+    ax.plot(odmr_data['Frequency'][lower_bound:upper_bound]/1e6,
+            slopes[i] * odmr_data['Frequency'][lower_bound:upper_bound] + intercepts[i], color='orange', linestyle='--')
 
 ax.set_ylabel("Voltage [V]")
-ax.set_xlabel("Frequency [Hz]")
+ax.set_xlabel("Frequency [MHz]")
 ax.set_title("Fitting ZCSs")
-ax.grid()
+#ax.grid()
+ax.legend()
 fig.tight_layout()
 fig.show()
 
@@ -77,8 +78,8 @@ sample_rate = int(len(times) / duration)  # [Hz]
 
 # apply conversion factor to the data and scale voltages in data_dict_x.value[0] by factor voltage_LIA_output_scaling
 # to account for the scaling of the analog LIA output -> that goes into the NIDAQ for the slope measurements
-samples_x_B_field = voltage_LIA_output_scaling * data_dict_x.value[0] * volts_to_nT_multiplication_factor
-samples_x_B_field_original = voltage_LIA_output_scaling * samples_x_B_field.copy()
+samples_x_B_field = data_dict_x.value[0] * volts_to_nT_multiplication_factor * voltage_LIA_output_scaling
+samples_x_B_field_original = samples_x_B_field.copy()
 
 # # remove the DC offset in each 1 second interval separately -> unsure if this is good practice
 # for i in range(int(result['/duration'][0])):
@@ -95,13 +96,13 @@ samples_x_B_field_original = voltage_LIA_output_scaling * samples_x_B_field.copy
 fig, axs = plt.subplots(2, 1)
 
 axs[0].plot(times[0:int(sample_rate * int(duration))], samples_x_B_field_original[0:int(sample_rate * int(duration))],
-            linewidth=0.2)
+            linewidth=0.2, alpha=0.8, color="dimgray")
 axs[0].grid()
 axs[0].set_title(f"{int(duration)} s Time Trace of Magnetic Field Noise")
 axs[0].set_xlabel("Time [s]")
 axs[0].set_ylabel("Magnetic Field [nT]")
 
-axs[1].plot(times[0:int(sample_rate)], samples_x_B_field[0:int(sample_rate)], linewidth=0.75)
+axs[1].plot(times[0:int(sample_rate)], samples_x_B_field[0:int(sample_rate)], linewidth=0.75, color="dimgray")
 axs[1].grid()
 axs[1].set_title("1 s Time Trace of Magnetic Field Noise")
 axs[1].set_xlabel("Time [s]")
@@ -119,12 +120,12 @@ sensitivity_nT_root_Hz = np.mean(
     [np.std(samples_x_B_field[i * int(sample_rate):(i + 1) * int(sample_rate)]) for i in
      range(int(duration))]) / np.sqrt(2 * f_ENBW)
 
-ax.plot(welch_x_hanning[0], np.sqrt(welch_x_hanning[1]), label="Hann window", alpha=0.7, linestyle="--")
+ax.plot(welch_x_hanning[0], np.sqrt(welch_x_hanning[1]), label="Hann window", alpha=0.7, linestyle="--", color="dimgray")
 ax.plot(welch_x_boxcar[0], np.sqrt(welch_x_boxcar[1]), label="Boxcar window", alpha=0.7, linestyle="-.")
 ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_title(f"Amplitude spectral density of the x-component of the magnetic field\n"
-             f"Sensitivity: {sensitivity_nT_root_Hz:.2f} pT/sqrt(Hz)")
+             f"Sensitivity: {sensitivity_nT_root_Hz:.2f} nT/Hz)")
 ax.set_xlabel("Frequency [Hz]")
 ax.set_ylabel("Sqrt of power spectral density [nT/sqrt(Hz)]")
 ax.grid()
