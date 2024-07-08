c0911650e3f896d5b1a4fdea8d4918039d91f5a4
Johannes Wesseler <johannes.wesseler@gmail.com>
2024-07-08 16:14:24 +0200

    improved hyperfine fitting.py

--- my_software/tools/fitting.py
+++ my_software/tools/fitting.py
@@ -47,8 +47,8 @@ def evaluate_hyperfine(frequency_array, voltage_array, feature_distance_in_Hz=0.
     if not isinstance(voltage_array, np.ndarray) and isinstance(voltage_array, pd.core.series.Series):
         voltage_array = np.array(voltage_array)
 
-    Hz_per_sample = (frequency_array[-1] - frequency_array[0]) / len(frequency_array)
-    min_feature_distance_in_samples = feature_distance_in_Hz / Hz_per_sample
+    frequency_spacing = (frequency_array[-1] - frequency_array[0]) / len(frequency_array)
+    min_feature_distance_in_samples = feature_distance_in_Hz / frequency_spacing
 
     peaks_indices, _ = find_peaks(voltage_array, height=0.02, distance=min_feature_distance_in_samples,
                                   prominence=min_feature_amplitude)
@@ -64,8 +64,8 @@ def evaluate_hyperfine(frequency_array, voltage_array, feature_distance_in_Hz=0.
     # Fit straight lines around the zero crossings
     for zero_crossing in zero_crossings_indices:
         # Define the range for fitting
-        fit_start = zero_crossing - int(zero_crossings_fit_range / Hz_per_sample)
-        fit_end = zero_crossing + int(zero_crossings_fit_range / Hz_per_sample)
+        fit_start = zero_crossing - int(zero_crossings_fit_range / frequency_spacing)
+        fit_end = zero_crossing + int(zero_crossings_fit_range / frequency_spacing)
 
         # Get the frequencies and voltages around the zero crossing
         frequencies_around_zero = frequency_array[fit_start:fit_end]
@@ -86,11 +86,229 @@ def evaluate_hyperfine(frequency_array, voltage_array, feature_distance_in_Hz=0.
 
 
 def fit_hyperfine(frequency_array, voltage_array, feature_distance_in_Hz=0.5e6, min_feature_amplitude=0.02,
+                  min_feature_height=0.02, use_peak_finding_if_fitting_fails=True, n_most_prominent_peaks, zero_crossings_fit_range=0.1e6, feature_fit_range=0.2e6, plot_fitting=False):
+    """
+    This function finds the peaks and dips in the ODMR signal and fits straight lines around the zero crossings.
+    It is ment for high-quality hyperfine ODMR spectra, where the peaks/dips can be identified by the maximum/minimum
+    values instead of having to perform fits. Compared to evaluate_hyperfine, this function fits parabolas to the peaks
+    and dips to more accurately determine their positions.
+
+    Args:
+        frequency_array (ndarray): Array of frequencies
+        voltage_array (ndarray): Array of voltages
+        feature_distance_in_Hz (float): The minimum distance between separate peaks/dips in Hz
+        min_feature_amplitude (float): The absolute of the minimum amplitude of a peak/dip
+        min_feature_height:
+        n_most_prominent_peaks: How many peaks should be found, only considers the n most prominent peaks
+        zero_crossings_fit_range (float): The range in Hz around the zero crossings to fit straight lines
+        feature_fit_range:
+        plot_fitting:
+
+    Returns:
+        peaks_indices (ndarray): Array indices of the peaks with respect to the frequency array
+        dips_indices (ndarray): Array indices of the dips with respect to the frequency array
+        zero_crossings_indices (ndarray): Array indices of the zero crossings with respect to the frequency array
+        slopes (ndarray): Array of the slopes of the fitted lines
+        intercepts (ndarray): Array of the intercepts of the fitted lines
+
+
+    """
+    plot_fitting_initial = plot_fitting
+
+    # Check if input arrays are np.ndarray or pandas series
+    if not isinstance(frequency_array, np.ndarray) and isinstance(frequency_array, pd.core.series.Series):
+        frequency_array = np.array(frequency_array)
+    if not isinstance(voltage_array, np.ndarray) and isinstance(voltage_array, pd.core.series.Series):
+        voltage_array = np.array(voltage_array)
+
+    # -----------------------------------
+    # DETERMINING ROUGH FEATURE POSITIONS
+    # -----------------------------------
+
+    frequency_spacing = (frequency_array[-1] - frequency_array[0]) / len(frequency_array)
+    min_feature_distance_in_samples = feature_distance_in_Hz / frequency_spacing
+
+    # smooth data before peak finding, s.t. strong outliers do not disturb the peak finding
+    smoothed_voltage_array = smooth(voltage_array, 20)
+
+    peaks_indices, _ = find_peaks(smoothed_voltage_array, height=min_feature_height,
+                                  distance=int(min_feature_distance_in_samples),
+                                  prominence=min_feature_amplitude)
+    dips_indices, _ = find_peaks(-smoothed_voltage_array, height=min_feature_height,
+                                 distance=int(min_feature_distance_in_samples),
+                                 prominence=min_feature_amplitude)
+
+    # Plot the smoothed curve with the peaks and dips for TESTING PURPOSES
+    if plot_fitting:
+        fig, ax = plt.subplots()
+        ax.plot(frequency_array, smoothed_voltage_array, label="smoothed", zorder=2, alpha=0.75)
+        ax.plot(frequency_array, voltage_array, label="original", zorder=1, alpha=0.75)
+        ax.scatter(frequency_array[peaks_indices], smoothed_voltage_array[peaks_indices], color='red',
+                   label="peaks smoothed",
+                   zorder=3)
+        ax.scatter(frequency_array[dips_indices], smoothed_voltage_array[dips_indices], color='green',
+                   label="dips smoothed",
+                   zorder=3)
+        plt.legend()
+        fig.suptitle('smoothed curve is used for peak finding\n Original curve is used for fitting')
+        fig.tight_layout()
+        fig.show()
+
+
+    # -------------------------
+    # FITTING PARABOLAS TO DIPS
+    # -------------------------
+
+    fit_peak_positions = []
+    fit_peak_uncertainties = []
+    fit_dip_positions = []
+    fit_dip_uncertainties = []
+
+    fit_sample_range = int(feature_fit_range / frequency_spacing)
+    for peak_index, dip_index in zip(peaks_indices, dips_indices):
+
+        try:
+            frequencies_around_peak = frequency_array[peak_index - fit_sample_range:peak_index + fit_sample_range]
+            voltages_around_peak = voltage_array[peak_index - fit_sample_range:peak_index + fit_sample_range]
+            frequencies_around_dip = frequency_array[dip_index - fit_sample_range:dip_index + fit_sample_range]
+            voltages_around_dip = voltage_array[dip_index - fit_sample_range:dip_index + fit_sample_range]
+
+            # estimate curvature of parabola for fitting
+            estimated_curvature = np.sqrt(np.abs((voltages_around_peak[0] - voltages_around_peak[-1]) / (
+                    frequencies_around_peak[0] - frequencies_around_peak[-1])))
+
+            # fit parabola to peaks
+            peak_params, peaks_cov = curve_fit(parabola, frequencies_around_peak, voltages_around_peak,  # noqa
+                                               p0=[frequency_array[peak_index], -estimated_curvature,
+                                                   voltage_array[peak_index]])
+
+            # fit parabola to dips
+            dip_params, dips_cov = curve_fit(parabola, frequencies_around_dip, voltages_around_dip,  # noqa
+                                             p0=[frequency_array[dip_index], +estimated_curvature,
+                                                 voltage_array[dip_index]])
+
+            # Get the estimated positions of peaks and dips
+            peak_position = peak_params[0]
+            peak_uncertainty = np.sqrt(np.diag(peaks_cov))[0]
+            dip_position = dip_params[0]
+            dip_uncertainty = np.sqrt(np.diag(dips_cov))[0]
+
+            if abs(peak_position - frequency_array[peak_index]) > 0.5e6:
+                print(
+                    "Warning: Peak position is more than 0.5 MHz away from most prominent data point at estimated peak.")
+            if abs(dip_position - frequency_array[dip_index]) > 0.5e6:
+                print(
+                    "Warning: Dip position is more than 0.5 MHz away from most prominent data point at estimated dip.")
+
+        except Exception as fitting_exception:
+            print(str(fitting_exception))
+            print("Fitting failed. Peaks/Dips were detected at" + str(frequency_array[peak_index]),
+                  str(frequency_array[dip_index]))
+
+            if use_peak_finding_if_fitting_fails:
+                peak_position = frequency_array[peaks_indices[0]]
+                dip_position = frequency_array[dips_indices[0]]
+            else:
+                peak_position, dip_position = np.nan, np.nan
+                peak_uncertainty, dip_uncertainty = np.nan, np.nan
+
+            # plot data for which fitting failed
+            fig, ax = plt.subplots()
+            ax.scatter(frequencies_around_peak, voltages_around_peak, color='blue', alpha=0.3)
+            ax.scatter(frequencies_around_dip, voltages_around_dip, color='blue', alpha=0.3, label='data')
+            ax.legend()
+            fig.show()
+            plt.show()
+
+        try:
+            if plot_fitting:
+                fig, ax = plt.subplots()
+                ax.scatter(frequencies_around_peak / 1e6, voltages_around_peak, color='blue', alpha=0.3)
+                ax.scatter(frequencies_around_dip / 1e6, voltages_around_dip, color='blue', alpha=0.3, label='data')
+                ax.plot(frequencies_around_dip / 1e6, smooth(voltages_around_dip, 20), zorder=3, color="yellow",
+                        linewidth=3)
+                ax.plot(frequencies_around_peak / 1e6, smooth(voltages_around_peak, 20), zorder=3, color="yellow",
+                        label="smoothed data", linewidth=3)
+                ax.plot(frequencies_around_peak / 1e6, parabola(frequencies_around_peak, *peak_params), color='red',
+                        zorder=2, linewidth=2)
+                ax.plot(frequencies_around_dip / 1e6, parabola(frequencies_around_dip, *dip_params), color='red',
+                        label="parabolic fit", zorder=2, linewidth=2)
+                ax.scatter(peak_position / 1e6, parabola(peak_position, *peak_params), color='green', zorder=3)
+                ax.scatter(dip_position / 1e6, parabola(dip_position, *dip_params), color='green', zorder=3)
+                ax.set_title("Fitting of Hyperfine Peaks and Dips")
+                ax.set_xlabel("Frequency [MHz]")
+                ax.set_ylabel("Voltage [V]")
+                ax.legend()
+                fig.show()
+                plt.show()
+
+        except Exception as PlottingException:
+            print("EXCEPTIONfailed for peak at index", peak_index, "and dip at index", dip_index)
+            print(str(PlottingException))
+
+        fit_peak_positions.append(peak_position)
+        fit_dip_positions.append(dip_position)
+        fit_peak_uncertainties.append(peak_uncertainty)
+        fit_dip_uncertainties.append(dip_uncertainty)
+
+    try:
+        linewidths = np.sqrt(3) * (frequency_array[peaks_indices] - frequency_array[dips_indices])
+    except Exception as LineWidthException:
+        print(str(LineWidthException))
+        linewidths = np.nan
+
+
+    # -----------------------------------
+    # DETERMINE ZERO CROSSINGS AND SLOPES
+    # -----------------------------------
+    zero_crossings_frequencies = np.array(fit_dip_positions/2 + fit_peak_positions / 2)
+
+
+    # Initialize lists to store the slope and intercept of the fitted lines
+    slopes = []
+    intercepts = []
+
+    # Fit straight lines around the zero crossings
+    for zero_crossing_f in zero_crossings_frequencies:
+        # Define the range for fitting
+        f_fit_start = zero_crossing_f - zero_crossings_fit_range
+        f_fit_end = zero_crossing_f + zero_crossings_fit_range
+        index_fit_start, index_fit_end = np.argmin(np.abs(frequency_array - f_fit_start)), np.argmin(np.abs(frequency_array - f_fit_end))
+
+        # Get the frequencies and voltages around the zero crossing
+        frequencies_around_zero = frequency_array[index_fit_start:index_fit_end]
+        voltages_around_zero = voltage_array[index_fit_start:index_fit_end]
+
+        # Fit a straight line to the data
+        slope, intercept = np.polyfit(frequencies_around_zero, voltages_around_zero, 1)
+
+        # Store the slope and intercept
+        slopes.append(slope)
+        intercepts.append(intercept)
+
+
+
+
+
+    return {
+        "peak_positions [Hz]": fit_peak_positions,
+        "peak_uncertainties [Hz]": fit_peak_uncertainties,
+        "dip_positions [Hz]": fit_dip_positions,
+        "dip_uncertainties [Hz]": fit_dip_uncertainties,
+        "linewidths [Hz]": linewidths,
+        "zero_crossing_frequencies [Hz]": zero_crossings_frequencies,
+        "zero_crossing_slopes [V/Hz]": slopes
+    }
+
+
+
+def fit_hyperfine_OLD(frequency_array, voltage_array, feature_distance_in_Hz=0.5e6, min_feature_amplitude=0.02,
                   min_feature_height=0.02, zero_crossings_fit_range=0.1e6, feature_fit_range=0.2e6, plot_fitting=False):
     """
     This function finds the peaks and dips in the ODMR signal and fits straight lines around the zero crossings.
     It is ment for high-quality hyperfine ODMR spectra, where the peaks/dips can be identified by the maximum/minimum
-    values instead of having to perform fits.
+    values instead of having to perform fits. Compared to evaluate_hyperfine, this function fits parabolas to the peaks
+    and dips to more accurately determine their positions.
 
     Args:
         frequency_array (ndarray): Array of frequencies
@@ -123,8 +341,8 @@ def fit_hyperfine(frequency_array, voltage_array, feature_distance_in_Hz=0.5e6, 
     # DETERMINING ROUGH FEATURE POSITIONS
     # -----------------------------------
 
-    Hz_per_sample = (frequency_array[-1] - frequency_array[0]) / len(frequency_array)
-    min_feature_distance_in_samples = feature_distance_in_Hz / Hz_per_sample
+    frequency_spacing = (frequency_array[-1] - frequency_array[0]) / len(frequency_array)
+    min_feature_distance_in_samples = feature_distance_in_Hz / frequency_spacing
 
     # smooth data before peak finding, s.t. strong outliers do not disturb the peak finding
     smoothed_voltage_array = smooth(voltage_array, 20)
@@ -164,8 +382,8 @@ def fit_hyperfine(frequency_array, voltage_array, feature_distance_in_Hz=0.5e6, 
     # # Fit straight lines around the zero crossings
     # for zero_crossing in zero_crossings_indices:
     #     # Define the range for fitting
-    #     fit_start = zero_crossing - int(zero_crossings_fit_range / Hz_per_sample)
-    #     fit_end = zero_crossing + int(zero_crossings_fit_range / Hz_per_sample)
+    #     fit_start = zero_crossing - int(zero_crossings_fit_range / frequency_spacing)
+    #     fit_end = zero_crossing + int(zero_crossings_fit_range / frequency_spacing)
     #
     #     # Get the frequencies and voltages around the zero crossing
     #     frequencies_around_zero = frequency_array[fit_start:fit_end]
@@ -187,7 +405,7 @@ def fit_hyperfine(frequency_array, voltage_array, feature_distance_in_Hz=0.5e6, 
     fit_dip_positions = []
     fit_dip_uncertainties = []
 
-    fit_sample_range = int(feature_fit_range / Hz_per_sample)
+    fit_sample_range = int(feature_fit_range / frequency_spacing)
     for peak_index, dip_index in zip(peaks_indices, dips_indices):
 
         try:
